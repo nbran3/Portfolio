020ea68e203f6549b0aba986cdf6acb738a15906
nbran3 <106123749+nbran3@users.noreply.github.com>
2023-05-24 17:42:54 -0400

    Update PDFGenerator.py

--- Python/PDFGenerator.py
+++ Python/PDFGenerator.py
@@ -1,31 +1,58 @@
 import pandas as pd
+import glob
 from fpdf import FPDF
+from pathlib import Path
 
-pdf = FPDF(orientation='P', unit="mm", format='A4')
-pdf.set_auto_page_break(auto =False, margin=0)
+filepaths = glob.glob('invoices/*.xlsx')
+print(filepaths)
 
-df = pd.read_csv('topics.csv')
+for file in filepaths:
+   df = pd.read_excel(file, sheet_name='Sheet 1')
 
-for index, row in df.iterrows():
-    pdf.add_page()
+   pdf = FPDF(orientation= "P", unit = 'mm', format ='A4' )
+   pdf.add_page()
 
-    pdf.set_font(family='Times', style='B', size=24)
-    pdf.set_text_color(100,100,100)
-    pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1)
-    pdf.line(10,20,200,20)
+   filename = Path(file).stem
+   invoice_nr, date = filename.split('-')
 
-    pdf.ln(260)
-    pdf.set_font(family='Times', style ="I", size =8)
-    pdf.set_text_color(180,180,180)
-    pdf.cell(w=0,h=10,txt=row['Topic'], align = 'R')
+   pdf.set_font(family='Times', size =16, style='B')
+   pdf.cell(w=50, h=8, txt=f'Invoice nr.{invoice_nr}', ln=1)
 
-    for i in range(row["Pages"] - 1):
-        pdf.add_page()
+   pdf.set_font(family='Times', size =16, style='B')
+   pdf.cell(w=50, h=8, txt=f'Date: {date}', ln=1)
 
-        pdf.ln(272)
+   df = pd.read_excel(file, sheet_name='Sheet 1')
 
-        pdf.set_font(family='Times', style="I", size=8)
-        pdf.set_text_color(180, 180, 180)
-        pdf.cell(w=0, h=10, txt=row['Topic'], align='R')
 
-pdf.output('output.pdf')
+   columns = list(df.columns)
+   columns = [item.replace("_"," ").title() for item in columns]
+   pdf.set_font(family="Times", size=10)
+   pdf.cell(w=30, h=8, txt=columns[0], border=1)
+   pdf.cell(w=70, h=8, txt=columns[1], border=1)
+   pdf.cell(w=30, h=8, txt=columns[2], border=1)
+   pdf.cell(w=30, h=8, txt=columns[3], border=1)
+   pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+
+   for index, row in df.iterrows():
+      pdf.set_font(family = "Times", size =10)
+      pdf.set_text_color(80,80,80)
+      pdf.cell(w=30, h=8, txt=str(row['product_id']), border=1)
+      pdf.cell(w=70, h=8, txt=str(row['product_name']), border=1)
+      pdf.cell(w=30, h=8, txt=str(row['amount_purchased']), border=1)
+      pdf.cell(w=30, h=8, txt=str(row['price_per_unit']), border=1)
+      pdf.cell(w=30, h=8, txt=str(row['total_price']), border=1, ln=1)
+
+   total_sum = df['total_price'].sum()
+   pdf.set_font(family="Times", size=10)
+   pdf.set_text_color(80, 80, 80)
+   pdf.cell(w=30, h=8, txt='', border=1)
+   pdf.cell(w=70, h=8, txt='', border=1)
+   pdf.cell(w=30, h=8, txt='', border=1)
+   pdf.cell(w=30, h=8, txt='', border=1)
+   pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+   pdf.set_font(family="Times", size=10, style='B')
+   pdf.cell(w=30, h=8, txt=f'The total price is {total_sum}', ln=1)
+
+
+   pdf.output(f'PDF/{filename}.pdf')
